a61e5729eb6bfe176b074eda60532b56057268cb
LazTech <laznology.dev@gmail.com>
2026-01-08 19:32:34 +0700

    fix(module): ensure DrizzleModule is imported in all feature modules

--- src/tunnel/tunnel.module.ts
+++ src/tunnel/tunnel.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TunnelService } from './tunnel.service';
+import { DrizzleModule } from '../db/db.module';
 
 @Module({
+  imports: [DrizzleModule],
   providers: [TunnelService],
 })
 export class TunnelModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { DrizzleModule } from '../db/db.module';
 
 @Module({
+  imports: [DrizzleModule],
   providers: [UserService],
 })
 export class UserModule {}
